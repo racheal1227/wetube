bcca411db69ad178ac88b13db54261ca62d8a013
백지현 <racheal1227@gmail.com>
2022-11-02 17:07:17 +0900

    #3 INTRODUCTION TO EXPRESS #3.3 Responses

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import express from 'express';
+
+const PORT = 4000;
+const app = express();
+
+app.get('/', (req, res) => res.send('<div>Home Page</div>'));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
